c85a4dce1982ef76433ab9cea2faa6fdeab8ab59
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-24 10:35:24 -0700

    added creategenesisblock function.

--- assets/js/Blockchain.js
+++ assets/js/Blockchain.js
@@ -1,5 +1,16 @@
 const Block = require('./Block');
 
 export default class Blockchain {
+  constructor() {
+    this.chain = [ createGenesisBlock() ];
+  }
+
+  function createGenesisBlock() {
+    return new Block(0, new Date(), '', { message: "This is where all life began for this blockchain." });
+  }
+
+  function getGenesisBlock() {
+    return this.chain[0];
+  }
 
 }
