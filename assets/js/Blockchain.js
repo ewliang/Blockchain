6f4ef69c08f2f95827fe6258bcd8f6d8749bd852
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-24 14:29:11 -0700

    Added addBlock, getLatestBlock, and isChainValid functions.

--- assets/js/Blockchain.js
+++ assets/js/Blockchain.js
@@ -6,11 +6,33 @@ export default class Blockchain {
   }
 
   function createGenesisBlock() {
-    return new Block(0, new Date(), '', { message: "This is where all life began for this blockchain." });
+    return new Block(0, new Date(), '0', "The Genesis Block");
   }
 
   function getGenesisBlock() {
     return this.chain[0];
   }
 
+  function addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.generateHash();
+    this.chain.push(newBlock);
+  }
+
+  function getLatestBlock() {
+    return this.chain[this.chain.length];
+  }
+
+  function isChainValid() {
+    for(let i = 1; i < this.chain[this.chain.length]; i++) { //Start at index 1 since 0 is genesis block.
+      let currentBlock = this.chain[i];
+      let previousBlock = this.chain[i - 1];
+      if(currentBlock.hash != currentBlock.generateHash) {
+        return false;
+      }
+      if(currentBlock.previousHash != previousBlock.hash) {
+        return false;
+      }
+    }
+  }
 }
