8548033725a46ada9999779ef3c58e72b5eaaced
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-25 11:47:02 -0700

    Test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Block = require('./assets/js/Block');
+const Blockchain = require('./assets/js/Blockchain');
 
+var myBlockchain = new Blockchain();
 
 var bk = new Block(0, '01-01-2018', 'sdfsdfsdf123', { hello: 'world' });
-console.log(bk.hash);
+myBlockchain.addBlock(bk);
